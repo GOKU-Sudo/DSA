0cbd27312d507f9a3d75f0151dbc915375d9c031
Purshotam Singh <purushotam321x@gmail.com>
2024-10-13 00:15:53 +0530

    updated

--- DSA/questions/diameterOfTree.cpp
+++ DSA/questions/diameterOfTree.cpp
@@ -13,23 +13,37 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+lass Solution {
 public:
 
-   int height(TreeNode* root){
-    if(root==NULL) return 0;
-    return 1+max(height(root->left),height(root->right));
-   }
+pair<int,int> heightDiameter(TreeNode* root){
+    if(root==NULL){
+        pair<int,int> p;
+        p.first=0;
+        p.second=0;
+        return p;
+    }
 
+    pair<int,int> leftAns=heightDiameter(root->left);
+    pair<int,int> rightAns=heightDiameter(root->right);
+
+    int leftHeight=leftAns.first;
+    int leftDiameter=leftAns.second;
+
+    int rightHeight=rightAns.first;
+    int rightDiameter=rightAns.second;
+
+    int height=1+max(leftHeight,rightHeight);
+    int diameter=max(leftHeight+rightHeight,max(leftDiameter,rightDiameter));
+
+    pair<int,int> finalAnsPair;
+    finalAnsPair.first=height;
+    finalAnsPair.second=diameter;
+    return finalAnsPair;
+}
 
     int diameterOfBinaryTree(TreeNode* root) {
-
-        if(root==NULL) return 0;
-
-        int option1= height(root->left)+height(root->right);
-        int option2= diameterOfBinaryTree(root->left);
-        int option3= diameterOfBinaryTree(root->right);
-
-        return max(option1,max(option2,option3));
+        pair<int,int>p=heightDiameter(root);
+        return p.second;
     }
 };
