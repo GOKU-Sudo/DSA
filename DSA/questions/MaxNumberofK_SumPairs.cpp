28eef4fd480c8097513892930b2037a166774717
Purshotam Singh <purushotam321x@gmail.com>
2024-12-12 13:59:22 +0530

    updated

--- DSA/questions/MaxNumberofK_SumPairs.cpp
+++ DSA/questions/MaxNumberofK_SumPairs.cpp
@@ -26,6 +26,7 @@
 // - Remove the first two 3's, then nums = [1,4,3]
 // There are no more pairs that sum up to 6, hence a total of 1 operation.
 //
+//
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
