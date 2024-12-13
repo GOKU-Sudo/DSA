d3fe9043da80824099d7667548c20cf594e7c1ba
PURSHOTAM SINGH <purushotam321x@gmail.com>
2024-12-13 23:13:58 +0530

    updated

---
+++ DSA/questions/ProductofArrayExceptSelf.cpp
@@ -0,0 +1,46 @@
+// 238. Product of Array Except Self
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result;
+
+        int product=1;
+
+        for(auto i: nums){
+            product=product*i;
+            result.push_back(product);
+        }  
+
+        product=1;
+        
+        for(int i=nums.size()-1;i>0;i--){
+            result[i]=result[i-1]*product;
+            product=product*nums[i];
+        }
+
+        result[0]=product;
+        return result;
+
+    }
+};
+ 
