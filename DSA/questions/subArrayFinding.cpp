7800294cdad897a155d3a6b8f179f237767a8e27
Purshotam Singh <purushotam321x@gmail.com>
2025-01-31 15:00:00 +0530

    updated

--- DSA/questions/subArrayFinding.cpp
+++ DSA/questions/subArrayFinding.cpp
@@ -3,7 +3,7 @@
 // Given an array A of size N that contains only non-negative integers, find a continuous sub-array that adds to a given number S and return the left and right indexes of that sub-array. In the case of multiple subarrays, return the subarray indexes that come first when moving from left to right.
 
 // Note: Both the indexes in the array should be according to 1-based indexing. You have to return an array list consisting of two elements, left and right. If no such subarray exists, return an array consisting of element -1.
-
+//
 
 
 #include<bits/stdc++.h>
